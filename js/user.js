074a259d2681ed26ac782ba210443827ff63e6cd
AnnaMatsieva <ann.matsieva@gmail.com>
2017-11-22 11:38:27 +0300

    some chsnges

--- js/user.js
+++ js/user.js
@@ -1,10 +1,5 @@
-// Get the modal
 var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
